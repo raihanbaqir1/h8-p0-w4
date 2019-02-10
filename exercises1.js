feec18b6e1d39943ecb364f27eff2fa5222a0085
Raihan <raihanbaqir@gmail.com>
2019-02-10 14:59:03 +0700

    add exercises1

---
+++ exercises1.js
@@ -0,0 +1,20 @@
+function angkaPrima(angka){
+    // var result = false;
+    // var pembagi = 235;
+    if(angka%1===0 && angka%angka===0){
+        if(angka%2===0 && angka%3===0){
+            return false;
+        } else{
+            return true;
+        }
+    }
+     return result;
+}
+    
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
