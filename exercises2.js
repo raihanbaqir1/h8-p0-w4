9870538de0aba084bdd9226058811053f30179a1
Raihan <raihanbaqir@gmail.com>
2019-02-11 12:55:04 +0700

    add exercises3 median

--- exercises2.js
+++ exercises2.js
@@ -1,5 +1,5 @@
 function fpb(angka1, angka2) {
-    var pembagi = 1;
+    var pembagi = 0;
     var faktor = 0;
     var faktor2 = 0;
     if(angka1 < angka2){
@@ -10,19 +10,22 @@ function fpb(angka1, angka2) {
         faktor2 = angka1;
     }
     // console.log(faktor);
+    var result = 0;
     while(pembagi < faktor){
+        // result = [];
+        pembagi = pembagi + 1
         if(faktor%pembagi===0 && faktor2%pembagi===0){
-            pembagi = pembagi + 1
-            console.log(pembagi);
+            result = pembagi;
+            // console.log(result)
         }   
     }
             
-    return pembagi;
+    return result;
   }
   
   // TEST CASES
   console.log(fpb(12, 16)); // 4
-//   console.log(fpb(50, 40)); // 10
-//   console.log(fpb(22, 99)); // 11
-//   console.log(fpb(24, 36)); // 12
-//   console.log(fpb(17, 23)); // 1
+  console.log(fpb(50, 40)); // 10
+  console.log(fpb(22, 99)); // 11
+  console.log(fpb(24, 36)); // 12
+  console.log(fpb(17, 23)); // 1
