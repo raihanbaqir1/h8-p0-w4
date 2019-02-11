1648a8bdb871c1a6d8a85916c05fbc7d59a35a97
Raihan <raihanbaqir@gmail.com>
2019-02-11 20:53:41 +0700

    change

--- exercises2.js
+++ exercises2.js
@@ -24,8 +24,9 @@ function fpb(angka1, angka2) {
   }
   
   // TEST CASES
-  console.log(fpb(12, 16)); // 4
-  console.log(fpb(50, 40)); // 10
-  console.log(fpb(22, 99)); // 11
-  console.log(fpb(24, 36)); // 12
-  console.log(fpb(17, 23)); // 1
+//   console.log(fpb(12, 16)); // 4
+//   console.log(fpb(50, 40)); // 10
+//   console.log(fpb(22, 99)); // 11
+//   console.log(fpb(24, 36)); // 12
+//   console.log(fpb(17, 23)); // 1
+  console.log(fpb(11, 99)); // 11
