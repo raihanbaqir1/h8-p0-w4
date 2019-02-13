4406d51bbe3d170b2f073c4950ea888d973256fa
Raihan <raihanbaqir@gmail.com>
2019-02-13 17:04:39 +0700

    bilangan prima

--- exercises1.js
+++ exercises1.js
@@ -7,7 +7,10 @@ function angkaPrima(angka){
     if(angka%1===0 && angka%angka===0){
         if(angka%2===0 && angka%3===0){
             return false;
-        } else if(angka%angka===0 && angka%3===0){
+        } else if(angka%5===0){
+            return false;
+        }
+        else if(angka%angka===0 && angka%3===0){
             return false;
         } else if(angka%angka===0 && angka%2===0){
             return false;
@@ -25,4 +28,4 @@ console.log(angkaPrima(3)); // true
 console.log(angkaPrima(7)); // true
 console.log(angkaPrima(6)); // false
 console.log(angkaPrima(23)); // true
-console.log(angkaPrima(33)); // false
+console.log(angkaPrima(25)); // false
